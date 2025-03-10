7d46c6e77937c31358977ee9ed281ba03b549e6a
Константин Леончик <109114228+Leo-id@users.noreply.github.com>
2025-03-10 20:16:48 +0300

    Homework2

---
+++ src/main/java/com/konstantinleonchik/HomeWork2/Car.java
@@ -0,0 +1,20 @@
+package com.konstantinleonchik.HomeWork2;
+
+public class Car {
+        String name;
+        int speed=0;
+        int max_speed;
+
+        public Car(String name, int mam_speed) {
+            this.name = name;
+            this.max_speed = mam_speed;
+        }
+
+        public int acceleration (int acceleration_pedal) throws WeakEngineException {
+            if(max_speed>=(speed+acceleration_pedal)){
+                speed+=acceleration_pedal;
+                System.out.println("Ускорились до: "+speed);
+                return speed;
+            }else throw new WeakEngineException("Двигатель слишком слабый, создайте новый автомобиль");
+        }
+}

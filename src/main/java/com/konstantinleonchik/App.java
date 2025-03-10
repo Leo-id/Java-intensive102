08b7adc3b6e5b5ed8410aeaf5ff801c1278defd6
Константин Леончик <109114228+Leo-id@users.noreply.github.com>
2025-03-10 20:08:58 +0300

    first commit

---
+++ src/main/java/com/konstantinleonchik/App.java
@@ -0,0 +1,13 @@
+package com.konstantinleonchik;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
